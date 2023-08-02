054b3e665e23c5ca4ba2b2cfcc0b048bae0fdcce
sahilkuthe <sahilkkuthe2004@gmail.com>
2023-08-02 21:27:42 +0530

    Create user activity layout created an integrated onCLick inside login activity

--- app/src/main/java/com/thegreatsahil/clancomm/LoginActivity.kt
+++ app/src/main/java/com/thegreatsahil/clancomm/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.thegreatsahil.clancomm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun login_login_btn_clicked(view: View){
+
+    }
+    fun login_signup_btn_clicked(view : View){
+        val signupIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(signupIntent)
     }
 }
